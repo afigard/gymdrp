2fff9942caf3aae9126344c55d9b975812b7725b
Adrien Figard <97508163+afigard@users.noreply.github.com>
2023-07-01 19:45:13 -0700

    Statistics page replaced by Deals page

--- script.js
+++ script.js
@@ -103,41 +103,119 @@ window.addEventListener("scroll", function () {
 
 // MAIN PAGE CANVAS
  if(document.querySelector('title').text == "GYMDRP"){
-    var c = document.getElementById('c');
-    var cxt = c.getContext("2d");
-    
-    var chinese = "田由甲申甴电甶男甸甹町画甼甽甾甿畀畁畂畃畄畅畆畇畈畉畊畋界畍畎畏畐畑";
-    chinese = chinese.split("");
-    
-    var font_size =10;
-    var columns = c.width/font_size; 
-    
-    var drops = [];
-    
-    for(var x=0;x<columns;x++){
-    drops[x]=1;
-    }
-    
-    function draw(){
-    cxt.fillStyle="rgba(0,0,0,0.05)";
-    cxt.fillRect(0,0,c.width,c.height);
-    
-    cxt.fillStyle = "#4c64ff8a";
-    cxt.font = font_size+'px arial';
-    
-    
-    for(var i=0;i<drops.length;i++){
-        var text = chinese[Math.floor(Math.random()*chinese.length)];
-        cxt.fillText(text,i*font_size,drops[i]*font_size);
-        
-        if(drops[i]*font_size>c.height && Math.random() >0.975)
-        drops[i]=0;
-        
-        drops[i]++;
-    }
-    
-    }
-    setInterval(draw,50);
+    (function() {
+        var COLORS, Confetti, NUM_CONFETTI, PI_2, canvas, confetti, context, drawCircle, i, range, resizeWindow, xpos;
+      
+        NUM_CONFETTI = 350;
+      
+        COLORS = [[85, 71, 106], [69, 61, 174], [56, 143, 219], [68, 94, 244], [70, 248, 236]];
+      
+        PI_2 = 2 * Math.PI;
+      
+        canvas = document.getElementById("c");
+      
+        context = canvas.getContext("2d");
+      
+        window.w = 0;
+      
+        window.h = 0;
+      
+        resizeWindow = function() {
+          window.w = canvas.width = 1150;
+          return window.h = canvas.height = 600;
+        };
+      
+        window.addEventListener('resize', resizeWindow, false);
+      
+        window.onload = function() {
+          return setTimeout(resizeWindow, 0);
+        };
+      
+        range = function(a, b) {
+          return (b - a) * Math.random() + a;
+        };
+      
+        drawCircle = function(x, y, r, style) {
+          context.beginPath();
+          context.arc(x, y, r, 0, PI_2, false);
+          context.fillStyle = style;
+          return context.fill();
+        };
+      
+        xpos = 0.5;
+      
+        window.requestAnimationFrame = (function() {
+          return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback) {
+            return window.setTimeout(callback, 1000 / 60);
+          };
+        })();
+      
+        Confetti = (function() {
+          function Confetti() {
+            this.style = COLORS[~~range(0, 5)];
+            this.rgb = "rgba(" + this.style[0] + "," + this.style[1] + "," + this.style[2];
+            this.r = ~~range(2, 6);
+            this.r2 = 2 * this.r;
+            this.replace();
+          }
+      
+          Confetti.prototype.replace = function() {
+            this.opacity = 0;
+            this.dop = 0.03 * range(1, 4);
+            this.x = range(-this.r2, w - this.r2);
+            this.y = range(-20, h - this.r2);
+            this.xmax = w - this.r;
+            this.ymax = h - this.r;
+            this.vx = range(0, 2) + 8 * xpos - 5;
+            return this.vy = 0.7 * this.r + range(-1, 1);
+          };
+      
+          Confetti.prototype.draw = function() {
+            var ref;
+            this.x += this.vx;
+            this.y += this.vy;
+            this.opacity += this.dop;
+            if (this.opacity > 1) {
+              this.opacity = 1;
+              this.dop *= -1;
+            }
+            if (this.opacity < 0 || this.y > this.ymax) {
+              this.replace();
+            }
+            if (!((0 < (ref = this.x) && ref < this.xmax))) {
+              this.x = (this.x + this.xmax) % this.xmax;
+            }
+            return drawCircle(~~this.x, ~~this.y, this.r, this.rgb + "," + this.opacity + ")");
+          };
+      
+          return Confetti;
+      
+        })();
+      
+        confetti = (function() {
+          var j, ref, results;
+          results = [];
+          for (i = j = 1, ref = NUM_CONFETTI; 1 <= ref ? j <= ref : j >= ref; i = 1 <= ref ? ++j : --j) {
+            results.push(new Confetti);
+          }
+          return results;
+        })();
+      
+        window.step = function() {
+          var c, j, len, results;
+          requestAnimationFrame(step);
+          context.clearRect(0, 0, w, h);
+          results = [];
+          for (j = 0, len = confetti.length; j < len; j++) {
+            c = confetti[j];
+            results.push(c.draw());
+          }
+          return results;
+        };
+      
+        step();
+      
+      }).call(this);
  }
 
 
